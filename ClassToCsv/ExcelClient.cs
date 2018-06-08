0c2cd804912d5dc49443b58f3e33727efc9afe1d
AlPeFe <30435251+AlPeFe@users.noreply.github.com>
2018-06-08 09:26:28 +0200

    Tools

---
+++ ClassToCsv/ExcelClient.cs
@@ -0,0 +1,71 @@
+﻿using GamFacturacion.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamFacturacion.Common
+{
+    public class ExcelClient
+    {
+        string Path { get; set; }
+        
+        string FileName { get; set; }
+     
+        public ExcelClient(string path, string fileName)
+        {
+            Path = path;
+            FileName = FileName;
+        }
+
+        public string AddCsvProveedores(List<Proveedor> proveedores, bool requiresHeader)
+        {
+            string header = "";
+            string previousContent = "";
+            string csv = "";
+
+            if (!requiresHeader)
+            {
+                previousContent = OpenCsv();
+            }
+            else
+            {
+                header = String.Join(",", typeof(Proveedor).
+                   GetProperties().ToArray<string>()) + Environment.NewLine;
+            }
+
+            foreach (var item in proveedores)
+            {
+                csv += String.Join(";", item.GetType().GetProperties().Select(c=> c.GetValue(item, null)));   //creo que aqui al ser de una lista ya mete el environment new line el solo
+            }
+            var fileContent = header + previousContent + csv;
+
+            return fileContent;
+        }
+
+        private void SaveCsv(string csv)
+        {
+           
+
+
+        }
+
+        private string OpenCsv()
+        {
+            var path = Path + FileName;
+
+            var content = File.ReadAllText(@path);
+
+            return content;
+        }
+
+        private void DeleteCsv(string csv)
+        {
+
+
+        }
+    }
+}
---
+++ FTP Client/FTPClient.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamFacturacion.Common
+{
+    public class FTPClient
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Path { get; set; }
+       
+
+        public FTPClient(string username, string password, string path)
+        {
+            UserName = username;
+            Password = password;
+            Path = path;
+         
+        }
+
+        public FtpStatusCode DownloadFile(string fileName, string destinationPath) //descargo fichero de FTP aquí para editar y añadir nuevos valores
+        {
+            try
+            {
+                var path = Path + fileName;
+
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(@path);
+                request.KeepAlive = true;
+                request.UsePassive = true;
+                request.UseBinary = true;
+
+                request.Method = WebRequestMethods.Ftp.DownloadFile;
+                request.Credentials = new NetworkCredential(UserName, Password);
+
+                using (var reader = request.GetResponse().GetResponseStream())
+                {
+                    using (var fileContent = new StreamReader(reader))
+                    {
+                        using (var localFile = new StreamWriter(File.Create(destinationPath)))
+                        {
+                            localFile.Write(fileContent.ReadToEnd());
+                            localFile.Flush();
+                        }
+                    }
+                }
+
+            }catch(Exception ex)
+            {
+                return FtpStatusCode.ServiceNotAvailable;
+            }
+
+            return FtpStatusCode.CommandOK;
+        }
+
+        public FtpStatusCode UploadFile(string localPath, string uploadFileName)
+        {
+            try
+            {
+                var path = Path + uploadFileName;
+
+                using (var ftpClient = new WebClient())
+                {
+                    ftpClient.Credentials = new NetworkCredential(UserName, Password);
+                    ftpClient.UploadFile(@path, WebRequestMethods.Ftp.UploadFile, localPath);
+                }
+            }
+            catch
+            {
+                return FtpStatusCode.ServiceNotAvailable;
+            }
+
+            return FtpStatusCode.CommandOK;
+        }
+
+        private void DeleteFile(string ftpFilePath)
+        {
+            try
+            {
+                var path = ftpFilePath;
+
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(@path);
+                request.KeepAlive = true;
+                request.UsePassive = true;
+                request.UseBinary = true;
+
+                request.Method = WebRequestMethods.Ftp.DeleteFile;
+                request.Credentials = new NetworkCredential(UserName, Password);
+
+                var reader = request.GetResponse();       
+            }
+            catch (Exception ex)
+            {
+               
+            }
+        }
+    }
+}
